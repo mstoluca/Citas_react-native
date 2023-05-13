7d87d62b0dcd9439635bed6a80e2412019a9668b
Alan Montoya <mstoluca@gmail.com>
2023-05-12 18:51:26 -0600

    inicial

---
+++ App.tsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  Text,
+  View, 
+} from 'react-native';
+
+const  App = () =>{
+  return (
+      <SafeAreaView>
+        <View>
+          <Text >Adminisrtador de Citas{' '}
+            <Text >Veterinaria</Text>
+          </Text>
+          
+        </View>
+      </SafeAreaView>
+
+    );
+  };
+
+  export default App;
